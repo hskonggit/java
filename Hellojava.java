d6cd609eb92f17d3f0196b29f63577ceb4345ce3
hskonggit <135101428+hskonggit@users.noreply.github.com>
2023-05-31 16:12:41 +0900

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,15 @@
+package ch01;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.print("1");
+		System.out.printf("1");
+		System.out.println("Hello, Java");
+		System.out.println("1+1=?");
+		//system.out은 왜 붙지?
+		//print  printf  println  차이점이 뭐지?
+		//println은 줄바꿈.
+	}
+
+}
